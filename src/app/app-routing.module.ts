b4e5198c90727e83c5054d9ec621f0e359d889be
ajaysinghtomar <ajaysinghtomar1993@gmail.com>
2021-01-26 23:53:19 +0530

    fromevent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './include/header/header.component';
+import { FromEventComponent } from './observable/from-event/from-event.component';
+import { ListComponent } from './observable/list/list.component';
+import { ObservableComponent } from './observable/observable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ObservableComponent, children: [
+    {path: '', component: ListComponent},
+    {path: 'fromEvent', component: FromEventComponent}
+  ]}
+  // {path: "promise"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './include/header/header.component';
 import { FooterComponent } from './include/footer/footer.component';
+import { FromEventComponent } from './observable/from-event/from-event.component';
+import { ListComponent } from './observable/list/list.component';
+import { ObservableComponent } from './observable/observable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FromEventComponent,
+    ListComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule,
